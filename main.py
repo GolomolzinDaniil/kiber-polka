5e633ea753f845c1eb1e51f3d323171edc0e65e2
FedosDan2 <f.danilov@g.nsu.ru>
2026-03-06 11:34:40 +0000

    Made main.py allmost ready. Need to add func for meta-model

---
+++ main.py
@@ -0,0 +1,221 @@
+import gc
+import json
+import time
+import optuna
+import pyarrow
+
+import polars as pl
+import numpy as np
+
+from pathlib import Path
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from catboost import CatBoostClassifier, Pool
+from typing import List, Tuple, Dict
+import matplotlib.pyplot as plt
+
+
+train_path = 'data/train/train_main_features.parquet'
+target_path = 'data/train/train_target.parquet'
+
+def init_data_start(target_id: str):
+    # Этап 1: загрузка данных
+    train = pl.read_parquet(train_path).drop('customer_id')
+    target = pl.read_parquet(target_path).drop('customer_id')
+    target = target.select(target_id).to_series()
+
+    # Этап 2: разделяем на valid/train
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        train.to_pandas(),
+        target.to_numpy(), 
+        test_size=0.2, 
+        random_state=42
+    )
+
+    # Этап 3: очищаем чтобы не кончилась память
+    del train, target
+    gc.collect()
+
+    # Этап 4: выбираем cat_feature с ними работает catboost
+    cat_feature_names = [col for col in X_train.columns if col.startswith("cat_feature")]
+    X_train[cat_feature_names] = X_train[cat_feature_names].astype(str)
+    X_valid[cat_feature_names] = X_valid[cat_feature_names].astype(str)
+
+    # Этап 5: для catboost pool лучше
+    train_pool = Pool(X_train, y_train, cat_feature_names)
+    valid_pool = Pool(X_valid, y_valid, cat_feature_names)
+
+    return train_pool, valid_pool, X_train, y_train
+
+
+def objective(trial: optuna.Trial, train: Pool, valid: Pool):
+    params = {
+        'eval_metric': 'AUC',
+        'verbose': 50, # отчёт каждые N новых деревьев
+        'use_best_model': True,
+        'early_stopping_rounds': 50, # если метрика не растёт 50 деревьев, пруним
+        'task_type': 'GPU',
+        'thread_count': -1,  # кол-во используемых потоков(-1 макс)
+        'iterations': 1_200, # специально много, потому что ставка не на это
+        'depth': trial.suggest_int('depth', 4, 7),
+        'learning_rate': trial.suggest_float('learning_rate', .01, .3, log=True),
+        'l2_leaf_reg': trial.suggest_float('l2_leaf_reg', 1., 50., log=True),
+        #'subsample': trial.suggest_float('subsample', .5, 1.),
+        #'colsample_bylevel': trial.suggest_float('colsample_bylevel', .5, 1.),
+        'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 1, 50),
+        'random_strength': trial.suggest_int('random_strength', 1, 7),
+    }
+
+    model = CatBoostClassifier(**params)
+
+    model.fit(train, eval_set=valid)
+
+    best_score = model.get_best_score()['validation']['AUC']
+
+    # ДЛЯ ПРУНИНГА
+    # длина AUC-списка = кол-ву деревьев (iterations) ЛИБО пока не обрубится
+    scores = model.get_evals_result()['validation']['AUC']
+    for epoch, score in enumerate(scores, 1):
+        trial.report(score, epoch)
+
+        if trial.should_prune():
+            raise optuna.TrialPruned()
+    
+    # ДЛЯ OPTUNA
+    # потом можно переделать чтобы рисовать кривые обучения
+    return best_score
+
+
+def get_bst_par(train: Pool, valid: Pool):
+    study = optuna.create_study(
+        direction='maximize',
+        sampler=optuna.samplers.TPESampler(seed=42),
+        pruner=optuna.pruners.MedianPruner(
+            n_startup_trials=5, # кол-во честных попыток (без прнунинга)
+            n_warmup_steps=20
+        )
+    )
+    study.optimize(
+        lambda trial: objective(trial, train, valid),
+        n_trials=30,    # ограничение в 30попыток поиска параметров
+        gc_after_trial=True,
+    )
+    
+    return study.best_params
+
+
+def get_bst_features(best_params, train: Pool, valid: Pool):
+    model = CatBoostClassifier(
+        **best_params,
+        verbose=False
+    )
+    model.fit(train, eval_set=valid)
+    
+    # нужно отфильтровать и оставить top-N признаков
+    feature_importance = model.get_feature_importance(prettified=True)
+
+    # НУЖНО ОПРЕДЕЛИТЬ ОПТИМАЛЬНОЕ КОЛ-ВО ВАЖНЫХ ПРИЗНАКОВ
+    N = 150
+    best_features = feature_importance['FeatureId'].head(N).tolist()
+
+    return best_features
+
+
+def oof_one_target(best_features, best_params, X_train, y_train):
+    X_train = X_train[best_features]
+    category = [col for col in X_train.columns if col.startswith('cat_feature')]
+
+    kf = StratifiedKFold(n_splits=5, shuffle=True)
+
+    # samples * 1 <- вектор предсказаний для каждой строчки (1, потому что мы обучаем тут одну определенную модель)
+    # потом простэкуем их все и получим samples * 41, что и будет являться обучащими данными для второй модели
+    preds_model = np.zeros(len(X_train))
+
+    for train_idx, valid_idx in kf.split(X_train, y_train):
+        X_tr, X_val = X_train.iloc[train_idx], X_train.iloc[valid_idx]
+        y_tr, y_val = y_train[train_idx], y_train[valid_idx]
+
+        train_fold = Pool(data=X_tr, label=y_tr,cat_features=category)
+        valid_fold = Pool(data=X_val, label=y_val,cat_features=category)
+
+        model = CatBoostClassifier(
+            **best_params,
+            verbose=False
+        )
+        model.fit(train_fold, valid_fold)
+        preds_model[valid_idx] = model.predict_proba(valid_fold)[:,1]
+
+        del model, train_fold, valid_fold
+        gc.collect()
+
+    return preds_model, category 
+
+
+def save_snapshot(best_params, best_features, target_idx, X_train, y_train, category):
+    X_train = X_train[best_features]
+    model = CatBoostClassifier(
+        **best_params,
+        verbose=False
+    )
+    
+    X_all_train = Pool(data=X_train, label=y_train, cat_features=category)
+    model.fit(X_all_train)
+
+    Path('snapshots').mkdir(exist_ok=True)
+    Path('configs').mkdir(exist_ok=True)
+
+    model.save_model(f'shapshots/{target_idx}.cbm')
+
+    data = {
+        'target': target_idx,
+        'best_params': best_params,
+        'best_features': best_features,
+        'category': category
+    }
+    
+    with open(f'configs/{target_idx}.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+
+def main():
+    target = pl.read_parquet(target_path).drop('customer_id')
+    columns_tar = target.columns
+    
+    # очищаем память
+    del target
+    gc.collect()
+
+    oof_predictions = {} # словарь для предсказаний
+    idx_col = [0, 1] # таргеты которые хочешь обработать за сессию
+
+    for i in range(idx_col[0], idx_col[1]):
+        target_idx = columns_tar[i]
+        print(f"TARGET: {target_idx}")
+        time_target = time.time()
+        # Этап 1: получаем data
+        train_pool, valid_pool, X_train, y_train = init_data_start(target_idx)
+
+        # Этап 2: получаем лучшие гиперпараметры таргета
+        best_params = get_bst_par(train_pool, valid_pool)
+
+        # Этап 3: получаем лучшие фичи в датасете для таргета
+        best_features = get_bst_features(best_params, train_pool, valid_pool)
+
+        # Этап 4: получаем столбец предсказаний по target_id
+        preds_target, category = oof_one_target(best_features, best_params, X_train, y_train)
+
+        # Этап 5: добавляем результат предсказания по таргету в общий список
+        oof_predictions[target_idx] = preds_target
+
+        #Этап 6: сохраняем данные о модели
+        save_snapshot(best_params, best_features, target_idx, X_train, y_train, category)
+        
+        time_target = time.time() - time_target
+        print(f'Work time: {time_target}\n')
+
+    meta_df = pl.DataFrame(oof_predictions)
+    meta_df.write_parquet('meta_data.parquet')
+    print(f"Итоговая таблица: {meta_df.shape}")
+
+
+if __name__ == "__main__":
+    main()
